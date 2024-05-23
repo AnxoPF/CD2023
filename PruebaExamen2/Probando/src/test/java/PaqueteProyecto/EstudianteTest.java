258df76f1ea61017925fea9e29bc8e9df9bec134
AnxoPF <a23anxopf@iessanclemente.net>
2024-05-23 19:26:36 +0200

    ??

---
+++ PruebaExamen2/Probando/src/test/java/PaqueteProyecto/EstudianteTest.java
@@ -0,0 +1,20 @@
+package PaqueteProyecto;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class EstudianteTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
