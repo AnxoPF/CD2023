630c95d7c41a2380d378466a81d45f60fc2d0bc3
AnxoPF <a23anxopf@iessanclemente.net>
2024-05-24 00:34:00 +0200

    hum

--- UD04/PruebaRefactorizacion/ProyectoRefactorizar/src/test/java/Ej1/AnagramaTest.java
+++ UD04/PruebaRefactorizacion/ProyectoRefactorizar/src/test/java/Ej1/AnagramaTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class AnagramaTest{
+public class AnagramaTest{
 
     @Test
     public void testAnagramas(){
